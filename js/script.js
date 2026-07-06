c1ca9bfaa181910576f44f7d8cfa8ea9a00693b4
mutohikaru <95016699+mutohikaru@users.noreply.github.com>
2026-07-06 15:57:22 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ function moveCarousel(direction) {
     behavior: "smooth",
   });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  // HTMLで設定した目印のクラスを取得
+  const openBtns = document.querySelectorAll('.js-slide-open');
+  const closeBtns = document.querySelectorAll('.js-slide-close');
+  const overlay = document.querySelector('.js-slide-overlay');
+
+  // 【開く】対象のボックスが複数あっても全て機能するように forEach を使用
+  openBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      overlay.classList.add('is-active'); // スライドインさせる
+      document.body.classList.add('no-scroll'); // 背景のスクロールをロック
+    });
+  });
+
+  // 【閉じる】戻るボタンが押された時の処理
+  closeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      overlay.classList.remove('is-active'); // スライドアウトさせる
+      document.body.classList.remove('no-scroll'); // 背景のスクロールロックを解除
+    });
+  });
+});
